7b55ca4b00e5656dd11eebca7bc8a36ce88059f0
isha3024 <isha.gandhi3024@gmail.com>
2024-08-01 18:09:56 +0530

    Delete unnecessary file

--- app/redux/actions/PracticeAction.js
+++
@@ -1,9 +0,0 @@
-// import * as actions from '../Types'
-// import { ADD_TO_CART } from "../Types"
-
-// export const addToCart = (item) => {
-//   return {
-//     type: ADD_TO_CART,
-//     payload: item
-//   }
-// }
